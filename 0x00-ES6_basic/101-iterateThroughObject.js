cbcf776574e183e05990fbdceffe924cf284a42b
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-02 18:50:21 +0000

    Fix eslinet

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   let retStr = '';
   for (const idx in reportWithIterator) {
-    if (parseInt(idx) === reportWithIterator.length - 1) {
+    if (parseInt(idx, 10) === reportWithIterator.length - 1) {
       retStr += reportWithIterator[idx];
     } else {
       retStr += `${reportWithIterator[idx]} | `;
