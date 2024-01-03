2ad0f56ef276dea3054b8ccf136b177d967504e3
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-03 13:14:46 +0000

    eslint fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,11 +3,11 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   try {
     const [user, photo] = await Promise.all([createUser(), uploadPhoto()]);
-    return {user, photo};
+    return { user, photo };
   } catch (err) {
     return {
       user: null,
       photo: null,
-    }
+    };
   }
 }
