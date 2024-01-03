72da7d8b5c39d5f47ddf42910306335690ff5709
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-03 10:08:37 +0000

    Use .then alone without catch or finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  }))
-    .catch(() => new Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  }, () => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
 }
