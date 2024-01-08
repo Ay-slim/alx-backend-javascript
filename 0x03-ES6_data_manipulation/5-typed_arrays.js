84103d3f8ce81a034a1e35cceee5a0c1e8dc3d5e
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-08 20:23:59 +0000

    Ditch try catch and check length disparity to trigger error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range')
+  }
   const arrBuff = new ArrayBuffer(length);
   const typedIntArray = new Int8Array(arrBuff);
-  try {
-    typedIntArray[position] = value;
-  } catch (e) {
-    throw new Error('Position outside range');
-  }
-  return new DataView(arrBuff);
+  const dataView = new DataView(arrBuff);
+  typedIntArray[position] = value;
+  return dataView;
 }
