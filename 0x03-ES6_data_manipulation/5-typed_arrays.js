0d32ba734310c9d7bed8bafb0248bef23a71aaa5
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-08 20:25:10 +0000

    Fix linter error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length || position < 0) {
-    throw new Error('Position outside range')
+    throw new Error('Position outside range');
   }
   const arrBuff = new ArrayBuffer(length);
   const typedIntArray = new Int8Array(arrBuff);
