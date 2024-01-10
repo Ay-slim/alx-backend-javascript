e673aada3d317e25b9358a4706f805478b1a4dd9
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-10 23:31:27 +0000

    Student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -36,3 +36,32 @@ interface printTeacherFunction {
 
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
 console.log(printTeacher("John", "Doe"));
+
+interface StudentClassConstructor {
+  (firstName: string, lastName: string): void;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework: () => string;
+  displayName: () => string;
+}
+class StudentClass {
+  firstName: string
+  lastName: string
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working'
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
+
+const sampleStudentClass: StudentClassInterface = new StudentClass('Jane', 'Doe');
+console.log(sampleStudentClass.workOnHomework());
+console.log(sampleStudentClass.displayName());
